19adaf8b5d0801dc85e8de6c22edfbd6cf66a9df
Haider Bastian <bastian.haider@gmx.at>
2019-01-31 13:08:04 +0100

    tests run

--- stopwatch.h
+++ stopwatch.h
@@ -10,6 +10,7 @@
  * A stop watch to measure cpu time
  * ----------------------------------------------------------
  */
+ 
 #ifndef ___STOPWATCH_H
 #define ___STOPWATCH_H
 
@@ -37,5 +38,10 @@
 *** Care must be taken that we measure cpu time! Check out the function clock() and the constant
 *** CLOCKS_PER_SEC from time.h in order to implement the functions correctly.
 */
+void start_stopwatch();
+float elapsed_time();
+void stop_stopwatch();
+void clock();
+
 
 #endif
